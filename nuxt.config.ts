2b4e88f245e09bc0abf651d70cdcd7ea976ec9f0
bradpreston <bap5393@gmail.com>
2025-12-26 15:08:10 -0500

    test env before app start

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,19 @@
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import "./lib/env.ts";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/eslint'],
-  css: ['~/assets/css/main.css'],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
   },
   eslint: {
     config: {
-      standalone: false
-    }
-  }
-})
+      standalone: false,
+    },
+  },
+});
