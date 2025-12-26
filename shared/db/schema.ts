cc3b4096747460e63f28d835e4be4ff09167223b
bradpreston <bap5393@gmail.com>
2025-12-26 10:59:10 -0500

    update schema table to add insert and select types

--- shared/db/schema.ts
+++ shared/db/schema.ts
@@ -1,7 +1,7 @@
 import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const users = sqliteTable("users", {
+export const usersTable = sqliteTable("users", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   firstName: text("first_name").notNull(),
   lastName: text("last_name").notNull(),
@@ -11,7 +11,7 @@ export const users = sqliteTable("users", {
   updatedAt: integer("updated_at", { mode: "timestamp" }).notNull().default(sql`CURRENT_TIMESTAMP`),
 });
 
-export const games = sqliteTable("games", {
+export const gamesTable = sqliteTable("games", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   status: text("status", { enum: ["active", "complete"]}).notNull(),
   startedAt: integer("started_at", { mode: "timestamp" }).notNull().default(sql`CURRENT_TIMESTAMP`),
@@ -20,27 +20,39 @@ export const games = sqliteTable("games", {
   updatedAt: integer("updated_at", { mode: "timestamp" }).notNull().default(sql`CURRENT_TIMESTAMP`),
 });
 
-export const players = sqliteTable("players", {
+export const playersTable = sqliteTable("players", {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  gameId: integer("game_id").references(() => games.id),
-  userId: integer("user_id").references(() => users.id),
+  gameId: integer("game_id").references(() => gamesTable.id),
+  userId: integer("user_id").references(() => usersTable.id),
   createdAt: integer("created_at", { mode: "timestamp" }).notNull().default(sql`CURRENT_TIMESTAMP`),
   updatedAt: integer("updated_at", { mode: "timestamp" }).notNull().default(sql`CURRENT_TIMESTAMP`),
 });
 
-export const rounds = sqliteTable("rounds", {
+export const roundsTable = sqliteTable("rounds", {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  gameId: integer("game_id").references(() => games.id),
+  gameId: integer("game_id").references(() => gamesTable.id),
   createdAt: integer("created_at", { mode: "timestamp" }).notNull().default(sql`CURRENT_TIMESTAMP`),
   updatedAt: integer("updated_at", { mode: "timestamp" }).notNull().default(sql`CURRENT_TIMESTAMP`),
 });
 
-export const roundScores = sqliteTable("round_scores", {
+export const roundScoresTable = sqliteTable("round_scores", {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  roundId: integer("round_id").references(() => rounds.id),
-  playerId: integer("player_id").references(() => players.id),
+  roundId: integer("round_id").references(() => roundsTable.id),
+  playerId: integer("player_id").references(() => playersTable.id),
   score: integer("score", { mode: "boolean"}).notNull(),
   phaseCompleted: integer("phase_completed").notNull(),
   createdAt: integer("created_at", { mode: "timestamp" }).notNull().default(sql`CURRENT_TIMESTAMP`),
   updatedAt: integer("updated_at", { mode: "timestamp" }).notNull().default(sql`CURRENT_TIMESTAMP`),
 });
+
+export type InsertUser = typeof usersTable.$inferInsert;
+export type InsertGame = typeof gamesTable.$inferInsert;
+export type InsertPlayer = typeof playersTable.$inferInsert;
+export type InsertRound = typeof roundsTable.$inferInsert;
+export type InsertRoundScore = typeof roundScoresTable.$inferInsert;
+
+export type SelectUser = typeof usersTable.$inferSelect;
+export type SelectGame = typeof gamesTable.$inferSelect;
+export type SelectPlayer = typeof playersTable.$inferSelect;
+export type SelectRound = typeof roundsTable.$inferSelect;
+export type SelectRoundScore = typeof roundScoresTable.$inferSelect;
